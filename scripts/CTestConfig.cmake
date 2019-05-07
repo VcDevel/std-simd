8609b8f8040ca256a6a55edeff4f88db2a7a8af7
Matthias Kretz <kretz@kde.org>
2019-05-07 18:13:15 +0200

    Submit to its own dashboard
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- scripts/CTestConfig.cmake
+++ scripts/CTestConfig.cmake
@@ -1,9 +1,9 @@
-set(CTEST_PROJECT_NAME "Vc")
+set(CTEST_PROJECT_NAME "std-simd")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "lxwww53.gsi.de")
-set(CTEST_DROP_LOCATION "/submit.php?project=Vc")
+set(CTEST_DROP_LOCATION "/submit.php?project=std-simd")
 
 set(CTEST_DROP_SITE_CDASH TRUE)
 
